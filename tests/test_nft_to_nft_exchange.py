0be0a2fa15c6d9c9f87f290ea1e6f568303ae4fe
work <tnnnnnnnn2@gmail.com>
2021-09-07 20:28:47 +0300

    Added bid creation test.

--- tests/test_nft_to_nft_exchange.py
+++ tests/test_nft_to_nft_exchange.py
@@ -1,8 +1,58 @@
-from brownie.network.contract import ContractContainer
+"""
+    Testing NFT to NFT Exchange.
+"""
+from typing import Tuple
 import pytest
 
-from brownie import accounts
+from brownie import NFTToNFTExchange, FakeERC721, accounts
+from brownie.network.contract import ProjectContract
 
-pytest.fixture
+@pytest.fixture
 def exchange():
-    return accounts[0].deploy()
+    """ Creating instance of NFTToNFTExchange. """
+    return NFTToNFTExchange.deploy(600, {'from': accounts[0]})
+
+@pytest.fixture
+def create_tokens() -> Tuple[ProjectContract, ProjectContract]:
+    """ Creating instances of FakerERC721.  """
+    fake_erc721_1 = FakeERC721.deploy({'from': accounts[1]})
+    fake_erc721_2 = FakeERC721.deploy({'from': accounts[2]})
+
+    return (fake_erc721_1, fake_erc721_2)
+
+@pytest.fixture
+def mint_tokens(create_tokens) -> Tuple[str, str]:
+    """ Distribution of tokens. """
+    fake_erc721_1, fake_erc721_2 = create_tokens
+    fake_erc721_1.mint(13424, accounts[3])
+    fake_erc721_2.mint(25252, accounts[4])
+
+    return (fake_erc721_1.address, fake_erc721_2.address)
+
+def test_create_bid_and_check(exchange, mint_tokens) -> None:
+    """ Creating a bid and check the result. """
+    first_addr, second_addr = mint_tokens
+    expected_bid_data = (
+        1, 
+        first_addr,
+        second_addr,
+        13423,
+        25252,
+        3000,
+        False,
+        False,
+        False
+    )
+    
+    tx = exchange.createBid(
+        13423,
+        first_addr,
+        second_addr,
+        25252,
+        700,
+        {'from': accounts[3], 'value': 3000}
+    )
+    
+    bid = exchange.getTradeById(tx.return_value)
+    
+    assert bid == expected_bid_data
